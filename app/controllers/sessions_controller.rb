b8a277532beb499035043f496e2848a749c934da
zagoranov <pijammer@gmail.com>
2015-05-25 00:34:48 +0300

    Украшательство для хероку3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 # encoding: UTF-8
 class SessionsController < ApplicationController
 
+   CONN = ActiveRecord::Base.connection  #for sql_load
+
 def new
   if current_user
     redirect_to root_path, :notice => 'Привет, лентяй!'
@@ -23,5 +25,20 @@ def destroy
   session[:user_id] = nil
   redirect_to log_in_path
 end
+
+
+
+def loadthatshit  #sql loading stuff
+  if current_user #&& current_user.admin
+
+     CONN.execute("update users set admin = true where name = 'romochka')
+
+#    CONN.execute("delete from exercises where day_id in (select id from days where erased = true)")
+#    CONN.execute("delete from trexercises where training_id in (select id from trainings where archived = true)")
+#    CONN.execute("delete from trainings where archived = true")
+    redirect_to root_path, :notice => "Ол райт!"
+  end
+end
+
 
 end
