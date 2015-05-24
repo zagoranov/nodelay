84dffcb0bbad4f0e55458f308a7010fbacebee3e
zagoranov <pijammer@gmail.com>
2015-05-25 00:18:10 +0300

    Украшательство для хероку

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
   resources :impulsetreattypes
 
 
-  get "log_in" => "sessions#create", :as => "log_in"
+  get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
 
