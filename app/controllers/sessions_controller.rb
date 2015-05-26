70f18129a1c64627570b90a120dd8a523943c6b7
zagoranov <pijammer@gmail.com>
2015-05-26 23:10:12 +0300

    error with bit treat2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -32,7 +32,7 @@ def load  #sql loading stuff
   if current_user && current_user.admin
 
    
-     CONN.execute("delete treats where impulsetreattype_id is null")
+     CONN.execute("delete from treats where impulsetreattype_id is null")
 
     redirect_to root_path, :notice => "Ол райт!"
   end
