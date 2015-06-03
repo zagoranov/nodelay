dc1f5ed95fac2f6017746593d8bab0bce99df746
zagoranov <pijammer@gmail.com>
2015-06-03 23:22:38 +0300

    no impulse wo type

--- app/controllers/impulses_controller.rb
+++ app/controllers/impulses_controller.rb
@@ -13,39 +13,42 @@ end
 
 def create
   impulse = current_user.impulses.create(impulse_params)
-  impulse.save
-  current_user.score += 1
-  current_user.save
-  r = Random.rand(current_user.randchance) + 1
-  if r == current_user.randchance #урра!!! награда!!!!
-  	treat = impulse.treats.create()
-    rr = Random.rand(18) + 1
-    if rr < 10
-      treat.icon = "0" + rr.to_s
-    else
-      treat.icon = rr.to_s
-    end
-  	r = Random.rand(current_user.randchance * 2) + 1
-  	if r == current_user.randchance #урра!!! БОЛЬШАЯ награда!!!!
-      type = Impulsetreattype.where(small: false).where(erased: false).where('user_id in (?)', current_user.id).order("RANDOM()").first
-      if type
-  	    treat.impulsetreattype = type
+  if impulse.impulsetreattype != nil
+    impulse.save
+    current_user.score += 1
+    current_user.save
+    r = Random.rand(current_user.randchance) + 1
+    if r == current_user.randchance #урра!!! награда!!!!
+    	treat = impulse.treats.create()
+      rr = Random.rand(18) + 1
+      if rr < 10
+        treat.icon = "0" + rr.to_s
       else
-      	treat.impulsetreattype = impulse.impulsetreattype
-      end	
-  	  treat.save
-  	  #redirect_to root_path, :notice => 'Ого! БОЛЬШАЯ награда!' 
-  	else
-      treat.impulsetreattype = impulse.impulsetreattype
-  	  treat.save
-  	  #redirect_to root_path, :notice => 'Ого! Награда!' 
-  	end  
-  else
-    #redirect_to root_path, :notice => 'Отлично, чувак!'
-  end
+        treat.icon = rr.to_s
+      end
+  	  r = Random.rand(current_user.randchance * 2) + 1
+  	  if r == current_user.randchance #урра!!! БОЛЬШАЯ награда!!!!
+        type = Impulsetreattype.where(small: false).where(erased: false).where('user_id in (?)', current_user.id).order("RANDOM()").first
+        if type
+    	    treat.impulsetreattype = type
+        else
+        	treat.impulsetreattype = impulse.impulsetreattype
+        end	
+  	    treat.save
+  	    #redirect_to root_path, :notice => 'Ого! БОЛЬШАЯ награда!' 
+  	  else
+        treat.impulsetreattype = impulse.impulsetreattype
+  	    treat.save
+  	    #redirect_to root_path, :notice => 'Ого! Награда!' 
+  	  end  
+    #else
+      #redirect_to root_path, :notice => 'Отлично, чувак!'
+    end
+  end   
   respond_to do |format|
     format.js { render partial: 'imptrlistrefresh' }
   end
+
 end
 
 
