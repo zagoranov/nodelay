7073a8d1e06ef017baf48b7764838c17fb0fc98e
zagoranov <pijammer@gmail.com>
2016-02-23 11:56:30 +0300

    no registration

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Rails.application.routes.draw do
 
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
-  get "sign_up" => "users#new", :as => "sign_up"
+#  get "sign_up" => "users#new", :as => "sign_up"
 
   get "tldr" => "tasks#help", :as => "tldr"
   
